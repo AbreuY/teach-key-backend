896bcb703a3ca984ea34e8f03bca8bb185a1daa1
AbreuY <yolfran12@gmail.com>
2021-11-03 17:28:21 -0500

    login and register endpoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+from logging import raiseExceptions
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,13 +9,17 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import Professor, db, User, Student, Professor
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
+
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY')
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,15 +35,45 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# endpoint para crear el registro de usuario usando metodo post
+# Debe recibir un objeto JSON con los datos de correo, 
+# nombre de usuario, fecha de nacimiento, contraseña, país.
+@app.route('/register/student', methods=['POST'])
+def handle_register_student():
+    if request.json is None:
+        return jsonify({'message':'The request was invalid'}), 400
+    body = request.json
+    user = Student.create(body)
+    return jsonify(user.serialize()), 201
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/register/professor', methods=['POST'])
+def handle_register_professor():
+    if request.json is None:
+        return jsonify({'message':'The request was invalid'}), 400
 
-    return jsonify(response_body), 200
+    body = request.json
+    user = Professor.create(body)
+    return jsonify(user.serialize()), 201
 
+@app.route('/login/<string:role>', methods=['POST'])
+def handle_login(role):
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    user = None
+    if role == "student":
+        user = Student.query.filter_by(email=email, password=password).one_or_none()
+    else:
+        user = Professor.query.filter_by(email=email, password=password).one_or_none()
+
+    if user is None:
+        return ({"msg": "Something went wrong, please try again"}), 401
+    
+    token = create_access_token(identity=user.id)
+    return jsonify({"message": "Login successfully", 
+    "token": token}), 200
+
+
+    
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -5,15 +5,94 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    user_name = db.Column(db.String(80), unique=False, nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "user_name": self.user_name
             # do not serialize the password, its a security breach
+        }
+
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    user_name = db.Column(db.String(100), unique=True)
+    dob = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    secondary_email = db.Column(db.String(100), unique=True, nullable=True)
+    contact_methods = db.Column(db.String(100), nullable=True)
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_name":self.user_name
+        }
+
+    @classmethod
+    def create(cls, bubulala):
+        try:
+            new_user = cls(**bubulala)
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return None
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    user_name = db.Column(db.String(100), unique=True)
+    dob = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    favorites  = db.relationship('Favorites', backref='student', uselist=True)
+    
+    def serialize(self):
+        return {
+            "id":self.id,
+            "user_name":self.user_name
+        }
+    @classmethod
+    def create(cls, bubulala):
+        try:
+            new_user = cls(**bubulala)
+            db.session.add(new_user)
+            db.session.commit()
+            return new_user
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return None
+
+
+class Services(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250))
+    description = db.Column(db.String(300))
+    price = db.Column(db.Integer)
+    schedule = db.Column(db.Integer)
+    image = db.Column(db.String(100))
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer,db.ForeignKey('student.id'), nullable=False)
+    url = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    __table_args__ = (db.UniqueConstraint(
+        'student_id',
+        'url',
+        name='unique_fav_for_user'
+    ),)
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "url": self.url,
+            "id": self.id,
+            "favName": self.name
         }
